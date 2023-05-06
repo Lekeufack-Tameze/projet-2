f15ca23ea4bd3708781ea439ac32504709f9f3f6
Lekeufack-Tameze <117746562+Lekeufack-Tameze@users.noreply.github.com>
2023-05-06 06:11:34 +0100

    debut Exercice3

--- src/Exercice3.java
+++ src/Exercice3.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Exercice3 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entree n ");
+        n = sc.nextInt();
+        System.out.println("le carré de " + n + " est " + n * n);
+        System.out.println("le cube de " + n + " est " + n * n * n);
 }
+
+
+    }
